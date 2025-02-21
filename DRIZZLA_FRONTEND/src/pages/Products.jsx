1205b8544d60c7f1e117f90b90d301550724cfc7
richurichu <paulsonrichu@gmail.com>
2025-02-21 22:48:44 +0530

    added video component

--- DRIZZLA_FRONTEND/src/pages/Products.jsx
+++ DRIZZLA_FRONTEND/src/pages/Products.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import back from '../assets/Videos/background.mp4'
 
 const Products = () => {
-  return (
-    <div className='flex-center h-screen text-2xl'>Products</div>
-  )
+    return (
+        <>
+            <div className=" relative w-full h-[600px]">
+                <video
+                    src={back}
+                    className=" w-full h-full object-fill"
+                    loop
+                    autoPlay
+                    muted
+                ></video>
+            <div className="absolute bottom-0  w-full h-32 bg-gradient-to-t from-black/90 to-transparent"></div>
+
+            </div> 
+
+        </>
+    )
 }
 
 export default Products
