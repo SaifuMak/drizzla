0b3c494648d4f342b7831a29ed222dde506d6552
richurichu <paulsonrichu@gmail.com>
2025-03-27 22:58:20 +0530

    made the video container as vh

--- DRIZZLA_FRONTEND/src/componets/AnimatedVideo.jsx
+++ DRIZZLA_FRONTEND/src/componets/AnimatedVideo.jsx
@@ -341,7 +341,7 @@ const AnimatedVideo = ({ MobileVideo, DesktopVideo }) => {
 
 
     return (
-        <div className="w-full max-sm:h-[400px]  bor  h-[640px]  lg:h-screen  ">
+        <div className="w-full max-sm:h-[40vh]  bor  h-[640px]  lg:h-screen  ">
             <div
                 ref={outerVideoContainerRef}
                 className="relative w-full  max-sm:h-[300px] md:h-[700px] lg:h-[800px] xl:h-[800px]  2xl:h-[900px]  "
