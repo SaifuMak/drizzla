eedd9187d8b42f53a802f40fdfdcdbb477229dfc
richurichu <paulsonrichu@gmail.com>
2025-03-28 19:03:02 +0530

    changed the animation

--- DRIZZLA_FRONTEND/src/componets/general/ServicesZigZagLayout.jsx
+++ DRIZZLA_FRONTEND/src/componets/general/ServicesZigZagLayout.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import { CgArrowLongRight } from "react-icons/cg";
 import useGsapFadeIn from '../../customHooks/useGsapFadeIn';
 import useGsapOpacity from '../../customHooks/useGsapOpacity';
+import ServicesImageZigZag from './ServicesImageZigZag';
+import ServicesContentZigZag from './ServicesContentZigZag';
 
 
 const ServicesZigZagLayout = ({ datas }) => {
@@ -11,23 +13,24 @@ const ServicesZigZagLayout = ({ datas }) => {
         <div className="flex justify-center ">
             <div className="my-2 xl:space-y-24 2xl:space-y-32">
                 {datas?.map((data, index) => {
-                    const contentCardRef = useGsapFadeIn(0, { initialPosition: 50, duration: 1, start: "top 65%" })
-                    const imageCardRef = useGsapFadeIn(0, { initialPosition: 50, duration: 1, start: "top 70%" })
+                    // const contentCardRef = useGsapFadeIn(0, { initialPosition: 50, duration: 1, start: "top 65%" })
+                    // const imageCardRef = useGsapFadeIn(0, { initialPosition: 50, duration: 1, start: "top 70%" })
 
                     return (
                         <div
                             key={index}
                             className="flex items-center my-16 text-white max-md:flex-col max-md:my-10"
                         >
-                            {/* <ServiceZigZagImage data={data} index={index} /> */}
+                            <ServicesImageZigZag data={data} index={index} />
 
-                            <div ref={imageCardRef} className={` hidden-text  bg-slate-900    2xl:w-[540px] 2xl:h-[450px] xl:w-[450px] xl:h-[400px] lg:w-[380px] lg:h-[350px] md:w-[320px] md:h-[280px]  w-full     rounded-3xl ${index % 2 !== 0 ? "md:order-1 md:mr-10  lg:mr-16" : "md:order-2 md:ml-10 lg:ml-16"}`}>
+                            {/* <div ref={imageCardRef} className={` hidden-text  bg-slate-900    2xl:w-[540px] 2xl:h-[450px] xl:w-[450px] xl:h-[400px] lg:w-[380px] lg:h-[350px] md:w-[320px] md:h-[280px]  w-full     rounded-3xl ${index % 2 !== 0 ? "md:order-1 md:mr-10  lg:mr-16" : "md:order-2 md:ml-10 lg:ml-16"}`}>
                                 <img src={data.image} alt={data.alt} className="z-0 object-cover w-full h-full max-md:object-cover rounded-3xl" />
-                            </div>
+                            </div> */}
 
                             {/* Reverse Order for Odd Index */}
+                            <ServicesContentZigZag data={data} index={index} />
 
-                            <div
+                            {/* <div
                                 ref={contentCardRef}
                                 className={`hidden-text  flex-1 flex-col max-sm:mt-4  w-full h-full  justify-center     rounded-2xl ${index % 2 !== 0 ? "md:order-2 lg:pl-12  2xl:pl-32 xl:pl-12" : "md:order-1"}`}
                             >
@@ -44,7 +47,7 @@ const ServicesZigZagLayout = ({ datas }) => {
                                     </ul>
                                 }
 
-                            </div>
+                            </div> */}
 
                         </div>)
                 })}
