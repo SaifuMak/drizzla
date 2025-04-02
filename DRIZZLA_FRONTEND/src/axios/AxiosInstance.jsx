bbdce5705250d575373a9b82798eb945afb4621b
richurichu <paulsonrichu@gmail.com>
2025-04-02 17:53:31 +0530

    ready to connect the backend

--- DRIZZLA_FRONTEND/src/axios/AxiosInstance.jsx
+++ DRIZZLA_FRONTEND/src/axios/AxiosInstance.jsx
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const Axiosinstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
-//   baseURL : 'https://app.cashplus.sc/',
+
+  // baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'http://139.59.5.223/',
+
 //   withCredentials: true, // Allows cookies to be sent with requests
 });
 
