042401ac6dda704271807a112a69b1392a111903
richurichu <paulsonrichu@gmail.com>
2025-04-02 10:01:05 +0530

    added  special condition for first section

--- DRIZZLA_FRONTEND/src/componets/general/ServicesZigZagLayout.jsx
+++ DRIZZLA_FRONTEND/src/componets/general/ServicesZigZagLayout.jsx
@@ -13,7 +13,9 @@ const ServicesZigZagLayout = ({ datas }) => {
             <div className="my-2 xl:space-y-24 2xl:space-y-32">
                 {datas?.map((data, index) => {
                     const contentCardRef = useGsapFadeIn(0, { initialPosition: 50, duration: 1, start: "top 68%" })
-                    const imageCardRef = useGsapFadeIn(0, { initialPosition: 50, duration: 1, start: "top 70%" })
+                    const contentFirstCardRef = useGsapFadeIn(0, { initialPosition: 50, duration: 1, start: "top 88%" })
+
+                    const imageCardRef = useGsapFadeIn(0, { initialPosition: 30, duration: 1, start: "top 70%" })
 
 
                     return (
@@ -31,10 +33,11 @@ const ServicesZigZagLayout = ({ datas }) => {
                             {/* Reverse Order for Odd Index */}
 
                             <div
-                                ref={contentCardRef}
+                                ref={index === 0 ? contentFirstCardRef :  contentCardRef }
                                 className={`hidden-text  flex-1 flex-col max-sm:mt-4  w-full h-full  justify-center     rounded-2xl ${index % 2 !== 0 ? "md:order-2 lg:pl-12  2xl:pl-32 xl:pl-12" : "md:order-1"}`}
                             >
                                 <h3 className="text-xl font-semibold md:text-2xl max-sm:ml-2 lg:text-3xl xl:text-4xl 2xl:w-10/12 ">{data.title}</h3>
+
                                 <p className='mt-2 font-light xl:mt-3 max-sm:font-extralight 2xl:tracking-wider max-sm:pl-2 sm:text-left lg:mt-5 md:mt-2 lg:w-10/12 xl:w-11/12 '>{data.description}</p>
                                 {data?.link && <Link to={data.link}><p className="flex items-center mt-4 cursor-default max-sm:ml-2 max-xl:text-lg-custom"><span className="flex items-center cursor-pointer group text-white/90 hover:text-white ">{data.linkText}<CgArrowLongRight className='ml-1 text-2xl transition-all duration-500 ease-in-out group-hover:translate-x-2 2xl:text-3xl group-hover:text-white text-white/80' /></span></p></Link>}
                                 {data?.features && <p className="mt-3 font-semibold tracking-wide xl:mt-5 max-sm:ml-2 xl:text-lg-custom ">{data.features.title}:</p>}
