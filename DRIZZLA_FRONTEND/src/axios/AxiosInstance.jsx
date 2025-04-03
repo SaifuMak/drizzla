3a6baa0ee28bdf7f473cefcaefaeac094cc4ba6b
richurichu <paulsonrichu@gmail.com>
2025-04-03 12:55:22 +0530

    backend domain updated

--- DRIZZLA_FRONTEND/src/axios/AxiosInstance.jsx
+++ DRIZZLA_FRONTEND/src/axios/AxiosInstance.jsx
@@ -3,7 +3,11 @@ import axios from 'axios';
 const Axiosinstance = axios.create({
 
   // baseURL: 'http://127.0.0.1:8000/',
-  baseURL: 'http://139.59.5.223/',
+  // baseURL: 'https://139.59.5.223/',
+  baseURL: 'https://mail.drizzla.com/',
+
+
+  
 
 //   withCredentials: true, // Allows cookies to be sent with requests
 });
